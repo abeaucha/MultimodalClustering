415fcc7ec0f8b89da36d030e27b7d8d10dc99c52
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-05-27 15:38:27 -0400

    Python packages

--- setup.sh
+++ setup.sh
@@ -12,3 +12,10 @@ ENV_NAME="multimodal-clust-env"
 # Create conda environment and install R
 echo -e "\nCreating environment..."
 conda create -n $ENV_NAME -c conda-forge r-base=4.4.2 python=3.12.10 -y
+
+# Install compiled R packages via conda (faster)
+echo -e "\nInstalling R packages..."
+#conda install -n $ENV_NAME -c conda-forge --file R_packages_conda.txt -y
+
+echo -e "\nInstalling python packages..."
+conda install -n $ENV_NAME -c conda-forge --file python_packages_conda.txt -y
